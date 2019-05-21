cfab5ce90588acd5c2d3a98567f8a7b265ce330b
Felipe Salome <felipesalome_ti@outlook.com>
2019-05-21 14:25:09 -0300

    Adicionado os comandos iniciais para o funcionamento do BOT no discord.

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log(`Bot iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`);
+    client.user.setActivity(`Albion Online`);
+});
+
+client.login(config.token);
